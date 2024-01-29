8c9ae2cbd45ec46fc74964fe355aafc421c91695
maririn100 <108440254+maririn100@users.noreply.github.com>
2024-01-29 11:58:51 +0900

    食べられるキノコを配置

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@ function main(param: g.GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
-		assetIds: ["player", "se"]
+		assetIds: ["player", "edible_mushroom", "se"]
 	});
 	scene.onLoad.add(() => {
 		// ここからゲーム内容を記述します
 
 		// 各アセットオブジェクトを取得します
 		const playerImageAsset = scene.asset.getImageById("player");
+		const edible_mushroom_ImageAsset = scene.asset.getImageById("edible_mushroom");
 		const seAudioAsset = scene.asset.getAudioById("se");
 
-		// プレイヤーを生成します
+		// プレイヤーを生成
 		const player = new g.Sprite({
 			scene: scene,
 			src: playerImageAsset,
@@ -19,16 +20,28 @@ function main(param: g.GameMainParameterObject): void {
 			height: playerImageAsset.height
 		});
 
-		// プレイヤーの初期座標を、X軸はちょっと左寄り、Y軸は画面の中心に設定
-		player.x = (g.game.width - player.width) / 4;
+		// 食べられるキノコを生成
+		const edible_mushroom = new g.Sprite({
+			scene: scene,
+			src: edible_mushroom_ImageAsset,
+			width: edible_mushroom_ImageAsset.width,
+			height: edible_mushroom_ImageAsset.height
+		});
+		// プレイヤーの初期座標を、X軸は右寄り、Y軸は画面の中心に設定
+		player.x = g.game.width - player.width;
 		player.y = (g.game.height - player.height) / 2;
-		player.onUpdate.add(() => {
-			// 毎フレームでY座標を再計算し、プレイヤーの飛んでいる動きを表現します
-			// ここではMath.sinを利用して、時間経過によって増加するg.game.ageと組み合わせて
-			// player.y = (g.game.height - player.height) / 2 + Math.sin(g.game.age % (g.game.fps * 10) / 4) * 10;
 
-			// プレイヤーの座標に変更があった場合、 modified() を実行して変更をゲームに通知します
+		// 食べられるキノコの初期座標
+		edible_mushroom.x = 0;
+		edible_mushroom.y = 20;
+
+		player.onUpdate.add(() => {
+			// 食べられるキノコを左→右に動かす
+			++edible_mushroom.x;
+
+			// プレイヤー・キノコの座標に変更があった場合、 modified() を実行して変更をゲームに通知
 			player.modified();
+			edible_mushroom.modified();
 		});
 
 		// 画面をタッチしたとき、SEを鳴らします⇒キノコとぶつかったときに食べる音にしたいので、参考としてコメントで置いておく
@@ -36,6 +49,7 @@ function main(param: g.GameMainParameterObject): void {
 		// 	seAudioAsset.play();
 		// });
 		scene.append(player);
+		scene.append(edible_mushroom);
 		// ここまでゲーム内容を記述します
 	});
 	g.game.pushScene(scene);
